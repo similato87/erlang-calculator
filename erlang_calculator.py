3e3abb70befb47dd61e416f5a9395864bd7afe12
Yuzhuo Zheng <yzheng10@unb.ca>
2023-12-07 22:46:10 -0400

    Initial commit

---
+++ erlang_calculator.py
@@ -0,0 +1,28 @@
+import math
+from decimal import Decimal
+
+def erlangB(A, N):
+    invB = 1.0
+    for i in range(1, N + 1):
+        invB = 1 + (i / A) * invB
+    return Decimal(1 / invB)
+
+def erlangC(A, N):
+    P = erlangB(A, N)
+    L = (A * P) / (N - A + (A * P))
+    return Decimal(L)
+
+def main():
+    print("Erlang B and C Calculator")
+    A = float(input("Enter traffic load (in Erlangs): "))
+    N = int(input("Enter the number of servers: "))
+
+    print("\nCalculating...")
+    B = erlangB(A, N)
+    C = erlangC(A, N)
+
+    print(f"Erlang B (Blocking Probability): {B:.5f}")
+    print(f"Erlang C (Delay Probability): {C:.5f}")
+
+if __name__ == "__main__":
+    main()
